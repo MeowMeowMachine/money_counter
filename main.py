f5c27475cd4d3c4c87778e8209e56301337e1764
ilovpython <135040458+MeowMeowMachine@users.noreply.github.com>
2025-08-06 11:52:30 +0200

    Added config variables

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-#######
+####### All changed jnto a contigater
 # Enter your income per hour below
 income = 15 # Income per hour
+goal = 3100
+progress = 300 # Load from config later
+Autoprogressload = true
+Moneysound = "C:foo"
+goalsound = "D;Boo"
+prosound = "L;moo"
+Key_start_stop = "AltF4"
+key_ProjEnd = "F18"
 
 ########
 
