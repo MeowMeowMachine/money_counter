d4557f30dc8d9431927f0e5d2922f83aba8a2d24
MeowMeowMachine <cookiefightersclan@web.de>
2025-08-06 22:22:50 +0200

    did stuff, next up; calculate n shit

--- main.py
+++ main.py
@@ -1,42 +1,51 @@
-####### All changed jnto a contigater
-# Enter your income per hour below
-income = 15 # Income per hour
-goal = 3100
-progress = 300 # Load from config later
-Autoprogressload = true
-Moneysound = "C:foo"
-goalsound = "D;Boo"
-prosound = "L;moo"
-Key_start_stop = "AltF4"
-key_ProjEnd = "F18"
+import json
 
-########
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-try: income = int(income)
-except ValueError:
-    print("Invalid input for income. Please enter a numeric value.")
-    exit(1)
-
+def load_config(config, Value=None, Value2=None):
+    try:
+        with open('config.json', 'r') as file:
+            data = json.load(file)
+            print(data)
+            # Check if the specified config exists in the loaded data
+            if config in data:
+                if Value is not None:
+                    # If a default value is provided, return it if the config is not found
+                    if Value2 is not None:
+                        return data[config].get(Value, Value2)
+                    else:
+                        return data[config].get(Value, None)
+                return data[config]
+            else:
+                # If the config is not found and no default value is provided, raise an error
+                raise KeyError(f"Config '{config}' not found in config.json")
+    except FileNotFoundError:
+        print("[CRITICAL ERROR]\n\n Error whilst loading config.json. Please ensure the file exists and is formatted correctly.\n\n")
+        exit(1)
+import time
 try: 
     import pygame
+    import keyboard
+
 except ImportError as e:
-    print(f"[CRITICAL ERROR] \n\n Please install pygame to run this program. \n\n Error: {e}")
+    print(f"[CRITICAL ERROR] \n\n Please install pygame & keyboard to run this program. \n\n Error: {e}")
+
+def 
 
 
+
+# load configs
+Currency = load_config("currency") # EUR
+Start_key = load_config("Keys", "StartStopSession")
+active = False
+# Start listening
+print(f"[INFO] Program activated; Listening for '{Start_key}' key to start counting.")
+while True:
+    if keyboard.is_pressed(Start_key):
+        active = not active
+        print(f"[INFO] '{Start_key}' key pressed; {'Starting' if active else 'Stopping'} count...")
+        # Wait for key release to avoid multiple toggles
+        while keyboard.is_pressed(Start_key):
+            pass
+    
+    if active:
+        time.sleep(0.1)  # Add a small delay to reduce CPU usage
+        
